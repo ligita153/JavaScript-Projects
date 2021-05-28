5158f735046653a20eabdd98c3cab470ef7bbaf4
ligita153 <82286939+ligita153@users.noreply.github.com>
2021-05-28 22:42:29 +0100

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+window.alert("Hello, everybody!");//Defining a window.alert()method to display a text
+document.write("Hello, hello!");//Defining a document.write() method to display a text
+document.write("<br>");//Inserting a line break
+document.write("She said, \"It's a beautiful morning.\"");//Defining a string that contains an apostrophe and quotations marks
+document.write("<br>");//Inserting a line break
+document.write("\"Don't dream your life,"
++ " live your dreams\"");//Concatenating a string
+document.write("<br>");//Inserting a line break
+var Summer = "July", Spring = "April", Autumn = "September", Winter = "December";//Assigning multiple variables in one statement
+var Summer = Summer.fontcolor("yellow"), Spring = Spring.fontcolor("green"), Autumn = Autumn.fontcolor("orange"), Winter = Winter.fontcolor("Blue");//Using the fontcolor method on str variables
+document.write(Autumn);//Displaying one of the variables using document.write method
+document.write("<br>");//Inserting a line break
+document.write(Winter);//Displaying one of the variables using document.write method
+document.write("<br>");//Inserting a line break
+document.write(Spring);//Displaying one of the variables using document.write method
+document.write("<br>");//Inserting a line break
+document.write(Summer);//Displaying one of the variables using document.write method
+document.write("<br>");//Inserting a line break
+document.write(9+11);//Defining a statement containing an expression
