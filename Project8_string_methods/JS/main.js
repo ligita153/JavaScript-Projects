958d84a010db08b917f450b033e20329fb9f79e5
ligita153 <82286939+ligita153@users.noreply.github.com>
2021-09-11 20:17:28 +0100

    Add files via upload

--- Project8_string_methods/JS/main.js
+++ Project8_string_methods/JS/main.js
@@ -1,24 +1,24 @@
-function full_quote() {
-    var part_1 = "Yesterday is history, ";
-    var part_2 = "tomorrow is a mystery, ";
-    var part_3 = "today is God's gift, ";
-    var part_4 = "that's why we call it the present.";
-    var whole_quote = part_1.concat(part_2, part_3, part_4);
-    document.getElementById("Concatenate").innerHTML = whole_quote;
+function full_quote() { //Defining a function and naming it
+    var part_1 = "Yesterday is history, "; //Defining a variable and giving it a value
+    var part_2 = "tomorrow is a mystery, "; //Defining a variable and giving it a value 
+    var part_3 = "today is God's gift, "; //Defining a variable and giving it a value
+    var part_4 = "that's why we call it the present."; //Defining a variable and giving it a value
+    var whole_quote = part_1.concat(part_2, part_3, part_4); //Defining a variable and giving it a value
+    document.getElementById("Concatenate").innerHTML = whole_quote; //Putting the value in order to display the result of the  function
 }
 
-function slice_Method(){
-    var Sentence = "If you fell down yesterday, stand up today.";
-    var Section = Sentence.slice(28,36);
-    document.getElementById("Slice").innerHTML = Section;
+function slice_Method(){ //Defining a function and naming it
+    var Sentence = "If you fell down yesterday, stand up today."; //Defining a variable and giving it a value
+    var Section = Sentence.slice(28,36); //Defining a variable and giving it a value
+    document.getElementById("Slice").innerHTML = Section; //Putting the value in order to display the result of the  function
 }
 
-function string_Method() {
-    var X = 153;
-    document.getElementById("Numbers_to_string").innerHTML = X.toString();
+function string_Method() { //Defining a function and naming it
+    var X = 153; //Defining a variable and giving it a value
+    document.getElementById("Numbers_to_string").innerHTML = X.toString(); //Putting the value in order to display the result of the  function
 }
 
-function precision_Method() {
-    var Y = 52163.6584952316584796325;
-    document.getElementById("Precision").innerHTML = Y.toPrecision(7);
+function precision_Method() { //Defining a function and naming it
+    var Y = 52163.6584952316584796325; //Defining a variable and giving it a value
+    document.getElementById("Precision").innerHTML = Y.toPrecision(7); //Putting the value in order to display the result of the  function
 }
